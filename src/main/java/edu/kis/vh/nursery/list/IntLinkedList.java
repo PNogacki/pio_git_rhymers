9da81ad1695a44022f2aef9247e4e7603a251a75
Hubert Kozłowski <255636@edu.p.lodz.pl>
2026-03-25 16:04:38 +0100

    udodanie zstalej EMPTY, dodanie funkcji czi czyszczenia wskaznikow przy usuwaniu elementow ze stosu

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    private static final int EMPTY = -1;
+
     Node last;
 
     public void push(int i) {
@@ -24,15 +26,20 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return EMPTY;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return EMPTY;
         int ret = last.value;
         last = last.prev;
+
+        if (last != null) {
+            last.next = null;
+        }
+
         return ret;
     }
 
