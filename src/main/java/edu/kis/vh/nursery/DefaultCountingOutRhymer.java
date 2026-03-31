cf87875fa74d77b59b0728f41a79721a5108ce38
Piotr Nogacki <255664@edu.p.lodz.pl>
2026-03-31 13:00:49 +0200

    17. dokumentacja klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa DefaultCountingOutRhymer implementuje prosty licznik oparty na stosie
+ * o stałej pojemności.
+ */
 public class DefaultCountingOutRhymer {
 
     public static final int CAPACITY = 12;
@@ -12,25 +16,54 @@ public class DefaultCountingOutRhymer {
         return total;
     }
 
+    /**
+     * Dodaje nową wartość do licznika, jeśli nie został osiągnięty limit
+     * pojemności.
+     * 
+     * @param in Wartość całkowita do dodania.
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza, czy licznik jest pusty.
+     * 
+     * @return true, jeśli licznik nie zawiera żadnych elementów; w przeciwnym razie
+     *         false.
+     */
     public boolean callCheck() {
         return total == EMPTY;
     }
 
+    /**
+     * Sprawdza, czy licznik osiągnął maksymalną pojemność.
+     * 
+     * @return true, jeśli tablica jest pełna; w przeciwnym razie false.
+     */
     public boolean isFull() {
         return total == CAPACITY - 1;
     }
 
+    /**
+     * Pozwala podejrzeć ostatnio dodaną wartość bez jej usuwania.
+     * 
+     * @return Ostatnia wartość w liczniku lub {@value #EMPTY}, jeśli licznik jest
+     *         pusty.
+     */
     protected int peekaboo() {
         if (callCheck())
             return EMPTY;
         return numbers[total];
     }
 
+    /**
+     * Pobiera i usuwa ostatnio dodaną wartość z licznika.
+     * 
+     * @return Ostatnia wartość lub {@value #EMPTY}, jeśli nie ma elementów do
+     *         pobrania.
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY;
